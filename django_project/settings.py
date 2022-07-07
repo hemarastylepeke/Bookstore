47079219fc87849a5927e3ca9864385b2bbbf874
hemarastylepeke <hemarastylpeke@gmail.com>
2022-07-07 14:00:58 -0400

    ch13

--- django_project/settings.py
+++ django_project/settings.py
@@ -129,6 +129,11 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+
+# media files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- django_project/urls.py
+++ django_project/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include  # new
 
@@ -9,4 +11,4 @@ urlpatterns = [
     # Local apps
     path("", include("pages.urls")),  # new for homepage
     path("books/", include("books.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
